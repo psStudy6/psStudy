da700144166f260e6ccf84acb4511a210b1a45c5
soram1119 <62399318+soram1119@users.noreply.github.com>
2022-07-16 15:16:12 +0900

    Update IjC209.java

--- src/week4/soram/IjC209.java
+++ src/week4/soram/IjC209.java
@@ -1,14 +1,16 @@
+package week4.soram;
 import java.util.*;
 //인프런 9.격자판 최대합
+//N*N 격자판에서 각 행의 합, 각 열의 합, 두 대각선의 합 중 가장 큰 합을 출력
 class IjC209 {  
 public int solution(int n,int[][] arr){
-	int answer = Integer.MIN_VALUE;
+	int answer = Integer.MIN_VALUE; //-2^31
 	int sum1,sum2;
 	for(int i=0;i<n;i++) {
 		sum1=sum2=0;
 		for(int j=0;j<n;j++) {
-			sum1 += arr[i][j];
-			sum2 += arr[j][i];
+			sum1 += arr[i][j];//각 행의 합
+			sum2 += arr[j][i];//각 열의 합
 		}
 		answer = Math.max(answer, sum1);
 		answer = Math.max(answer, sum2);
@@ -16,8 +18,8 @@ public int solution(int n,int[][] arr){
 	}
 	sum1=sum2=0;
 	for(int i=0;i<n;i++) {
-		sum1+=arr[i][i];
-		sum2+=arr[i][n-i-1];
+		sum1+=arr[i][i];//대각선의 합
+		sum2+=arr[i][n-i-1];//반대 대각선의 합
 	}
 	answer=Math.max(answer,sum1);
 	answer=Math.max(answer, sum2);
