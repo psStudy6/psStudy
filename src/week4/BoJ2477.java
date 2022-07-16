5d96ddfe1e50e3a1d4e14048bfaecfed5d516160
LeeJongbok <s2feeling@naver.com>
2022-07-17 02:29:55 +0900

    Update BoJ2477.java

--- src/week4/BoJ2477.java
+++ src/week4/BoJ2477.java
@@ -24,9 +24,14 @@ public class Main {
            dir[i] = direction;
            len[i] = length;
            
+           // direction이 1이나 2일 때는 동 or 서를 의미하고,
+           // 그중 최대값을 widthMax에 저장한다. 
            if(direction == 1 || direction == 2){
                widthMax = Math.max(widthMax, length);
-           }else{
+           }
+           // direction이 3이나 4일 때는 남 or 북을 의미하고,
+           // 그중 최대값을 heightMax에 저장한다. 
+           else{
                heightMax = Math.max(heightMax, length);
            }
        }
@@ -37,6 +42,9 @@ public class Main {
            int beforeDir = (i+5)%6;
            int nextDir = (i+1)%6;
            
+           // 이전 값의 방향과 이후 값의 방향이 같다면, 그 지점은 사각형의 움푹 패인 지점을 의미함
+           // 이 때, 방향에 따라서 방향이 1이나 2면 widthMin에
+           // 3이나 4면 heightMin에 값을 저장함 
            if(dir[beforeDir] == dir[nextDir]){
                if(dir[i] == 1 || dir[i] == 2){
                    widthMin = len[i];
@@ -46,7 +54,7 @@ public class Main {
            }
        }
        
-       
+       // 최종 결과를 구함
        answer = N*((widthMax*heightMax)-(widthMin*heightMin));
        
        System.out.println(answer);
