6d0c6ee0d494faf6567be84e8d19aa7a7584f5c9
soram1119 <62399318+soram1119@users.noreply.github.com>
2022-07-20 17:31:54 +0900

    Update IjC211.java

--- src/week5/soram/IjC211.java
+++ src/week5/soram/IjC211.java
@@ -22,26 +22,7 @@ public class IjC211 {
 		return answer;
 		
 	}
-	public int solution1(int n,int[][] arr) {
-		int answer=0,max=Integer.MIN_VALUE;//Integer.MIN_VALUE = -2147483648
-		for(int i=1;i<=n;i++) {
-			int cnt = 0;
-			for(int j=1;j<=n;j++) {
-				for(int k=1;k<=5;k++) {
-					if(arr[i][k]==arr[j][k]) {
-						cnt++;
-						break;
-					}
-				}
-			}
-			if(cnt>max) {
-				max=cnt;
-				answer=i;
-			}
-		}
-		return answer;
-		
-	}
+	
 	public static void main(String args[]) {
 		IjC211 T = new IjC211();
 		Scanner sc = new Scanner(System.in);
