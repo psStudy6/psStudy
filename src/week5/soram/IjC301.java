5a2be6cca33cbd5bdfaf1cdd7b1ce01158b5e3d3
soram1119 <62399318+soram1119@users.noreply.github.com>
2022-07-21 17:24:58 +0900

    Create IjC301.java

---
+++ src/week5/soram/IjC301.java
@@ -0,0 +1,33 @@
+package week5.soram;
+import java.util.*;
+//1.두 배열 합치기(two pointers algorithm)
+public class IjC301 {
+	public ArrayList<Integer> solution(int n,int m,int[] a,int[] b){
+		ArrayList<Integer> answer = new ArrayList<>();
+		int p1=0,p2=0;
+		while(p1<n && p2<m) {
+			if(a[p1]<b[p2]) answer.add(a[p1++]);
+			else answer.add(b[p2++]);
+		}
+		while(p1<n) answer.add(a[p1++]);
+		while(p2<m) answer.add(b[p2++]);
+		
+		return answer;
+	}
+	public static void main(String[] args) {
+		IjC301 T = new IjC301();
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] a = new int[n];
+		for(int i=0;i<n;i++) {
+			a[i]=sc.nextInt();
+		}
+		int m = sc.nextInt();
+		int[] b=new int[m];
+		for(int i=0;i<m;i++) {
+			b[i]=sc.nextInt();
+		}
+		for(int x:T.solution(n, m, a, b)) System.out.print(x+" ");
+	}
+
+}
