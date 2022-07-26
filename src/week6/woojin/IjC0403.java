6c0583a76923f867d7dc51a66952a03124bbf812
DESKTOP-K9D48KS\mycom <dnwlsrla40@naver.com>
2022-07-26 22:23:53 +0900

    [김우진] 2022-07-26 week6 IjC 문제 제출

---
+++ src/week6/woojin/IjC0403.java
@@ -0,0 +1,77 @@
+package week6.woojin;
+
+/*
+    Site : Infrean Java Coding Site
+    title : 매출액의 종류
+    number : 04강 03번
+    subject : HashMap
+    my velog link :
+ */
+
+import java.io.*;
+import java.util.HashMap;
+
+public class IjC0403 {
+    // 내가 푼 방식
+// public static void main(String[] args) throws IOException {
+// BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+// BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+// StringBuilder sb = new StringBuilder();
+// String[] input = br.readLine().split(" ");
+// int n = Integer.parseInt(input[0]);
+// int k = Integer.parseInt(input[1]);
+// int[] sales = new int[n];
+// input = br.readLine().split(" ");
+// for (int i = 0; i < n; i++) {
+// sales[i] = Integer.parseInt(input[i]);
+// }
+// HashMap<Integer, Integer> map = new HashMap<>();
+// for (int i = 0; i < k; i++) {
+// map.put(sales[i], map.getOrDefault(sales[i],0) + 1);
+// }
+// sb.append(map.size()).append(" ");
+// for (int i = k; i < n; i++) {
+// map.put(sales[i-k], map.get(sales[i-k]) - 1);
+// if(map.get(sales[i-k]) == 0)
+// map.remove(sales[i-k]);
+// map.put(sales[i], map.getOrDefault(sales[i], 0) + 1);
+// sb.append(map.size()).append(" ");
+// }
+// bw.write(sb.toString());
+// bw.flush();
+// bw.close();
+// br.close();
+// }
+
+    // 강의 방식 (two pointer) 크게 차이는 없는 듯
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        String[] input = br.readLine().split(" ");
+        int n = Integer.parseInt(input[0]);
+        int k = Integer.parseInt(input[1]);
+        int[] sales = new int[n];
+        input = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            sales[i] = Integer.parseInt(input[i]);
+        }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < k-1; i++) {
+            map.put(sales[i], map.getOrDefault(sales[i],0) + 1);
+        }
+        int lt = 0;
+        for (int rt = k-1; rt < n; rt++) {
+            map.put(sales[rt], map.getOrDefault(sales[rt], 0) + 1);
+            sb.append(map.size()).append(" ");
+            map.put(sales[lt], map.get(sales[lt]) - 1);
+            if(map.get(sales[lt]) == 0)
+                map.remove(sales[lt]);
+            lt++;
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
