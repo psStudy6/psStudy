c059c98bc4be4a6672c3812a738aeccf04ef8acc
gkfgran5037 <gkfgran5037@naver.com>
2022-09-03 10:04:46 +0900

    Update BoJ3184.java
    Site : 프로그래머스
    Title : 1844 / 게임 맵 최단거리
    Date : 1th September 2022 year
    Subject : BFS
    Success or False : ✔️(Success)
    Contents : This logic of my method and the side description of the
    method speaking

--- src/week11/yumi/PrG1844.java
+++ src/week11/yumi/PrG1844.java
@@ -1,6 +1,8 @@
 package week11.yumi;
 
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /** 프로그래머스 1844 게임 맵 최단거리
  * 레벨2
@@ -12,51 +14,138 @@ import java.io.IOException;
  * 		maps는 0과 1로만 이루어져 있으며, 0은 벽이 있는 자리, 1은 벽이 없는 자리
  * 		처음에 캐릭터는 게임 맵의 좌측 상단인 (1, 1) 위치에 있으며, 상대방 진영은 게임 맵의 우측 하단인 (n, m) 위치에 있습니다.
  * 
- * 결과 : 정확성: 69.9
- * 		효율성: 0.0 (효율성 테스트 4개 모두 통과 못함)
- * 		합계: 69.9 / 100.0
+ * 핵심 : bfs - 너비 우선 탐색 = 최단 거리
+ * 		변수명 조심하자!! 확인 또 확인! 조심 또 조심! ㅠ^ㅠ
 */
+
+
+// 위치 정보
+class Pos {
+	int x;
+	int y;
+	int cnt;
+	
+	public Pos(int x, int y, int cnt) {
+		this.x = x;
+		this.y = y;
+		this.cnt = cnt;
+	}
+}
+
 public class PrG1844 {
 	
-	public static int[][] postArr = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+	public static int[][] posArr = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 	public static boolean[][] visited;
 	public static int m, n, min;
 	
+	
+	/*방법 1 : bfs - 너비 우선 탐색 = 최단 거리 - queue
+	 * 정확성: 69.9
+	 * 효율성: 30.1
+	 * 합계: 100.0 / 100.0
+	*/
 	public int solution(int[][] maps) {
+		m = maps.length;
+		n = maps[0].length;
+		visited = new boolean[m][n];
+		Queue<Pos> que = new LinkedList<>();
+		// 항상 0, 0 에서 시작
+		que.offer(new Pos(0, 0, 1));
+	    visited[0][0] = true;
+	    
+		while (!que.isEmpty()){
+			Pos pos = que.poll();
+			int posX = pos.x;
+			int posY = pos.y;
+			// 팀 진영에 도착하면 최솟값 츨력
+			if (posX == m-1 && posY == n-1) {
+				return pos.cnt;
+			}
+			// 각 칸은 벽(-1)을 제외한 인접한 상하좌우 칸으로 이동 가능 
+			for (int i = 0; i < posArr.length; i++) {
+				int movePosX = posX + posArr[i][0];
+				int movePosY = posY + posArr[i][1];
+				// 범위 벗어날 시
+				if (!isRange(maps, movePosX, movePosY)) {
+					continue;
+				}
+				// 방문 표시 및 que 추가
+	            visited[movePosX][movePosY] = true;
+				que.offer(new Pos(movePosX, movePosY, pos.cnt + 1));
+			}
+		}
+		// 팀 전영에 도달할 방법 없을 시
+		return -1;
+	} 
+	
+	// 범위 체크
+	public boolean isRange(int[][] maps, int posX, int posY) {
+		// 필드 외 영역 제외
+		if (posX < 0 || posX >= m || posY < 0 || posY >= n) {
+			return false;
+		}
+		// 벽 또는 방문한 곳일 경우 제외
+		if (maps[posX][posY] == 0 || visited[posX][posY]){
+			return false;
+		}
+		return true;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*방법 2 : dfs - 깊이 탐색 후 비교하므로 효율성 떨어짐 - 범위가 넓을 때 실패
+	 *정확성: 69.9
+	 *효율성: 0.0 (효율성 테스트 4개 모두 통과 못함)
+	 *합계: 69.9 / 100.0 
+	 */
+	public int solution2(int[][] maps) {
 		m = maps.length;
         n = maps[0].length;
         visited = new boolean[m][n];
-        
 		min = Integer.MAX_VALUE;
-		dfs(maps, m-1, n-1, 1);
+		
+		dfs(maps, 0, 0, 1);
 		return  min == Integer.MAX_VALUE ? -1 : min;
 	}
 	
 	public void dfs(int[][] maps, int posX, int posY, int cnt) {
+		// 방문 체크
 		visited[posX][posY] = true;
-		if (posX == 0 && posY == 0) {
+		// 적 팀 진영 도달 시 최단 경로 세팅
+		if (posX == m-1 && posY == n-1) {
 			min = Math.min(min, cnt);
 			return;
 		}
-		
-		for (int i = 0; i < postArr.length; i++) {
-			int moveX = posX + postArr[i][0];
-			int moveY = posY + postArr[i][1];
+		// 4방향 탐색
+		for (int i = 0; i < posArr.length; i++) {
+			int moveX = posX + posArr[i][0];
+			int moveY = posY + posArr[i][1];
 			// 범위가 아닐 시 탐색 안 함
-			if (!isRange(maps, moveX, moveY)) {
+			if (!isRange2(maps, moveX, moveY)) {
 				continue;
 			}
+			// 탐색
 			dfs(maps, moveX, moveY, cnt+1);
+			// 미방문으로 변경
 			visited[moveX][moveY] = false;
 		}
+		// 4방향을 다 돌면 방문으로 체크
 		visited[posX][posY] = true;
 	}
 	
-	public boolean isRange(int[][] maps, int posX, int posY) {
-		// 범위가 아닌 경우 리턴
+	// 범위 체크
+	public boolean isRange2(int[][] maps, int posX, int posY) {
+		// 필드 외 영역 제외
 		if (posX < 0 || posX >= m || posY < 0 || posY >= n) {
 			return false;
 		}
+		// 벽 또는 방문한 곳일 경우 제외
 		if (visited[posX][posY] || maps[posX][posY] == 0) {
 			return false;
 		}
@@ -66,9 +155,8 @@ public class PrG1844 {
 	public static void main(String[] args) throws IOException {
 		PrG1844 prg = new PrG1844();
 		int[][] maps = {{1,0,1,1,1}, {1,0,1,0,1}, {1,0,1,1,1}, {1,1,1,0,1}, {0,0,0,0,1}};
-		int[][] maps2 = {{1,0,1,1,1}, {1,0,1,0,1}, {1,0,1,1,1}, {1,1,1,0,0}, {0,0,0,0,1}};
+		//int[][] maps2 = {{1,0,1,1,1}, {1,0,1,0,1}, {1,0,1,1,1}, {1,1,1,0,0}, {0,0,0,0,1}};
 		
-		System.out.println(prg.solution(maps2));
-		//System.out.println(prg.solution(maps2));
+		System.out.println(prg.solution2(maps));
 	}
 }
